88280bc11c6fabb894953891137a73d5df6797a1
SATISH KUMAR <satish.kumar@bancvue.com>
2017-02-04 22:09:40 -0600

    Initial version of s3 walker.

---
+++ README.md
@@ -0,0 +1,24 @@
+# search_buckets_for_keys
+* Use elasticsearch to index all version of all keys in all buckets in an AWS account.
+* Query elasticsearch for keys of interest.
+
+# Setup
+* Elasticsearch docker instance
+
+        mkdir -p esdata
+        docker run --name elasticsearch -p 9200:9200 -p 9300:9300 -d -v "$PWD/esdata":/usr/share/elasticsearch/data elasticsearch
+
+* Use s3_walker to walk all versions and write to elasticsearch.
+
+        import s3_walker
+        import elasticsearch
+        import boto3
+
+        session = boto3.session.Session(profile='sandbox')
+        client = session.client('s3')
+
+        es = elasticsearch.Elasticsearch()
+
+        walker = s3_walker.S3Walker(client, 50)
+        for version in walker.get_all_versions():
+            es.create(index="s3", doc_type="object", id=version['bucket']+'/'+version['version']['Key'], body=version)
---
+++ s3_walker.py
@@ -0,0 +1,27 @@
+class S3Walker(object):
+    def __init__(self, s3client, batch_size):
+        """
+        s3_client -- An instance of the s3 client object
+        batch_size -- The number of keys to fetch per request.
+        """
+        self.__s3_client = s3client
+        self.__batch_size = batch_size
+
+    def get_all_versions(self):
+        list_buckets_response = self.__s3_client.list_buckets()
+        buckets = list_buckets_response['Buckets']
+        for bucket in buckets:
+            bucket_name = bucket['Name']
+            list_object_versions_response = self.__s3_client.list_object_versions(Bucket=bucket_name, MaxKeys=self.__batch_size)
+            if list_object_versions_response.has_key('Versions'):
+                for version in list_object_versions_response['Versions']:
+                    yield {'bucket':bucket_name, 'version':version}
+            while list_object_versions_response['IsTruncated']:
+                list_object_versions_response = self.__s3_client.list_object_versions(
+                    Bucket=bucket_name,
+                    MaxKeys=self.__batch_size,
+                    KeyMarker=list_object_versions_response['NextKeyMarker'],
+                    VersionIdMarker=list_object_versions_response['NextVersionIdMarker'])
+                if list_object_versions_response.has_key('Versions'):
+                    for version in list_object_versions_response['Versions']:
+                        yield {'bucket':bucket_name, 'version':version}
